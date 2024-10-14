0ab5d74436e4294d372594d0d3eefdc8fa97e622
lee01-bit <jaleellee19@gmail.com>
2024-10-14 22:15:28 +0200

    Updated my appointment Form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import AboutUs from './pages/aboutUs/AboutUs';
+import Appointment from './pages/appointment/Appointment';
+
 
 function App() {
   return (
     <div className="App">
-      <AboutUs/>
+      {/* <AboutUs/> */}
+      {/* <Appointment/> */}
     </div>
+    
   );
 }
 
--- src/pages/appointment/Appointment.js
+++ src/pages/appointment/Appointment.js
@@ -1,7 +1,100 @@
 import React from 'react'
+import "./appointmentStyles.css"
+
 
 export default function Appointment() {
   return (
-    <div>Appointment</div>
+    <>
+      <div>
+        <h1 className='title'>Appointment Form</h1>
+      </div>
+    <div className="form">
+      
+      <htmlForm >
+      
+        <section>
+                    <div className="info-details">
+                        <span className="info-global"><label htmlhtml="name" >Name & Surname</label></span>
+                            <input className="info" id ="name" name="Name" type="text" placeholder="e.g John Doe" required/>
+                    </div>
+
+                    <div className="info-details">
+                        <span className="info-global"><label htmlFor="email" >Email</label></span>
+                            <input  id="email" className="info" name="email" type="email" placeholder="example@gmail.com"/>
+                    </div>
+
+                    <div className="info-details">
+                        <span className="info-global"><label htmlFor="phone" >Phone Number</label></span>
+                            <input id="phone" className="info" name="phone" type="tel" placeholder="e.g 098-098-5674" required/>
+                    </div>
+
+                    <div className="info-details">
+                        <span className="info-global"><label htmlFor="ID">Identification Number</label></span>
+                            <input id="ID" className="info" name="ID" type="text" placeholder="e.g 19435763782894" maxlength="13" required/>
+                    </div>
+
+                    <div className="info-details">
+                        <span className="info-global"><label  htmlFor="contact" >Contact Preference:</label></span>
+                        <div className="radio01">
+                            <div><label><input id="contact"  name="contact" type="radio"/> Phone</label></div>
+                            <div><label><input  name="contact" type="radio"/> Email</label></div>
+                        </div>
+                    </div> 
+
+                </section>
+  
+            {/* <!-- This part is htmlFor the Medical appointments  --> */}
+
+                <section className="appointment_info">
+                    <h2>Medical Departments of <span className="heading-htmlForm"> Appointments</span></h2>
+                        <p>Please Select Your Appointment:</p>  
+                    <div className="app01">
+                       <ul>
+                            <div className="appoint">
+                                <li className="app01"><input name="Appointment" type="radio"/> Cardiology</li>
+                                <li className="app01"><input name="Appointment" type="radio"/> Radiology</li>
+                                <li className="app01"><input name="Appointment" type="radio"/> Psychiatry</li>
+                                <li className="app01"><input name="Appointment" type="radio"/> Family Planning</li>
+                                <li className="app01"><input htmlFor="other" name="Appointment" type="radio"/> Other...</li>
+                            </div>
+                        
+                            <div className="appoint">
+                                <li className="app1"><input name="Appointment" type="radio"/> Allergic Diseases</li>
+                                <li className="app1"><input name="Appointment" type="radio"/> Physio</li>
+                                <li className="app1"><input name="Appointment" type="radio"/> Pediatrics</li>
+                                <li className="app1"><input name="Appointment" type="radio"/> INR</li>
+
+                            </div>
+                            
+                            <div>
+                                <li className="app1"><input name="Appointment" type="radio"/> Dermatology</li>
+                                <li className="app1"><input name="Appointment" type="radio"/> Disorders</li>
+                                <li className="app1"><input name="Appointment" type="radio"/> Infectious Diseases</li>
+                                <li className="app1"><input name="Appointment" type="radio"/> Nutrition & Dietician</li>
+                            </div>
+                        </ul>
+                        <div className="info-other">
+                            <textarea id="other"  rows="3" cols="0" placeholder="please specify"></textarea>
+                        </div>
+                    </div>
+
+
+              
+                </section>
+                
+               
+                    
+
+                <section >
+                    <div className="submit_button">
+                        <button type="submit">SCHEDULE APPOINTMENT</button>
+                    </div>
+                </section>
+                    
+      </htmlForm>
+
+     
+    </div>
+    </>
   )
 }
