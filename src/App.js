a0b131738fd89ae8220b70a52884564969c2dc38
Shabbeer Petersen <Shabbeer.Petersen@capaciti.org.za>
2024-10-15 15:46:25 +0200

    fixed app

--- src/App.js
+++ src/App.js
@@ -4,19 +4,6 @@ import './App.css';
 import AboutUs from './pages/aboutUs/AboutUs';
 
 import Appointment from './pages/appointment/Appointment';
-<<<<<<< HEAD
-import { UserFeedbackForm } from './pages/appointment/UserFeedbackform';
-
-
-function App() {
-  return (
-    <div className="App">
-      <AboutUs/>
-      {/* <Appointment/> */}
-      {/* <UserFeedbackForm/> */}
-    </div>
-    
-=======
 import PortalPage from './pages/portal/PortalPage';
 
 function App() {
@@ -28,7 +15,6 @@ function App() {
       <Route path='/PortalPage' element={<PortalPage/>}/>
     </Routes>
   </BrowserRouter>
->>>>>>> 54defbd2e1eba020afa0588de3bf4bd392ec4412
   );
 }
 
