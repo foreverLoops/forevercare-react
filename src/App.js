bd57e65f4e4103adfe19ab00d313594faa0d43a3
Lee01-bit <jaleellee19@gmail.com>
2024-10-15 15:49:36 +0200

    update

--- src/App.js
+++ src/App.js
@@ -3,31 +3,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import AboutUs from './pages/aboutUs/AboutUs';
 import Appointment from './pages/appointment/Appointment';
-<<<<<<< HEAD
-import { UserFeedbackForm } from './pages/appointment/UserFeedbackform';
-
-
-function App() {
-  return (
-    <div className="App">
-      {/* <AboutUs/> */}
-      {/* <Appointment/> */}
-      {/* <UserFeedbackForm/> */}
-    </div>
-    
-=======
 import PortalPage from './pages/portal/PortalPage';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/AboutUs' element={<AboutUs/>}/>
-      <Route path='/Appointment' element={<Appointment/>}/>
-      <Route path='/PortalPage' element={<PortalPage/>}/>
+      <Route path='/about' element={<AboutUs/>}/>
+      <Route path='/appointment' element={<Appointment/>}/>
+      <Route path='/portal' element={<PortalPage/>}/>
     </Routes>
   </BrowserRouter>
->>>>>>> 54defbd2e1eba020afa0588de3bf4bd392ec4412
   );
 }
 
